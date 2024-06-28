b4229569489085c5d7de5eec1618f6a220c87c7f
Milan <4k.milan15@gmail.com>
2024-06-28 15:56:57 +0500

    28.06.2024 - 15.56

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+let stepMarkerPlace = document.querySelector('.step_marker');
+let backButton  = document.querySelector('#control_button_1');
+let forwardButton  = document.querySelector('#control_button_4');
+let numberOfQuestion = 1; 
+let numberOfQuestionSum = 10;
+
+let windowWidth  = window.innerWidth;
+console.log(windowWidth)
+function adjustContentWrapper() {
+    const headerHeight = document.getElementById('header').offsetHeight;
+    const footerHeight = document.getElementById('footer').offsetHeight;
+    const contentWrapper = document.getElementById('contentWrapper');
+
+    contentWrapper.style.paddingTop = headerHeight + 'px';
+    contentWrapper.style.paddingBottom = footerHeight + 'px';
+}
+
+window.addEventListener('load', adjustContentWrapper);
+window.addEventListener('resize', adjustContentWrapper);
+
+for (let i = 0; i < numberOfQuestion; i++){
+    let markers = document.createElement('img');
+    markers.src = "./content/radio_button_blue.svg";
+    stepMarkerPlace.appendChild(markers);
+}
+
+for (let i = 0; i < numberOfQuestionSum-numberOfQuestion; i++){
+    let markers = document.createElement('img');
+    markers.src = "./content/radio_button.svg";
+    stepMarkerPlace.appendChild(markers);
+}
+
+window.addEventListener('resize',(e) => {
+    let windowWidth  = window.innerWidth;
+    if (windowWidth  <=  1000)  {
+        forwardButton.innerHTML  =  '<img src="./content/arrow_forward.svg" alt="<">';
+        backButton.innerHTML  =  '<img src="./content/arrow_back.svg" alt="<">';
+    } else {
+        backButton.textContent  =  'Назад';
+        forwardButton.textContent  =  'Далее';
+    }
+});
