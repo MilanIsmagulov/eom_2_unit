bae012a748a894cc961e853ba625b40a42fe0e19
Milan <4k.milan15@gmail.com>
2024-07-01 13:53:09 +0500

    add_phone_adaptation

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,14 @@
 let stepMarkerPlace = document.querySelector('.step_marker');
 let backButton  = document.querySelector('#control_button_1');
+let ansverButton  = document.querySelector('#control_button_2');
+let reloadButton  = document.querySelector('#control_button_3');
 let forwardButton  = document.querySelector('#control_button_4');
 let titleUpper = document.querySelector('#upper_title');
 let blackHeader  = document.querySelector('#header_text');
 let numberOfQuestion = 1; 
 let numberOfQuestionSum = 10;
-
 let windowWidth  = window.innerWidth;
-console.log(windowWidth)
-function adjustContentWrapper() {
-    const headerHeight = document.getElementById('header').offsetHeight;
-    const footerHeight = document.getElementById('footer').offsetHeight;
-    const contentWrapper = document.getElementById('contentWrapper');
 
-    contentWrapper.style.paddingTop = headerHeight + 'px';
-    contentWrapper.style.paddingBottom = footerHeight + 'px';
-}
-
-window.addEventListener('load', adjustContentWrapper);
-window.addEventListener('resize', adjustContentWrapper);
 
 for (let i = 0; i < numberOfQuestion; i++){
     let markers = document.createElement('img');
@@ -32,10 +22,18 @@ for (let i = 0; i < numberOfQuestionSum-numberOfQuestion; i++){
     stepMarkerPlace.appendChild(markers);
 }
 
+function shortenTitle(element, maxLength) {
+    let title = element.innerText;  // Получаем заголовок элемента
+    if (title.length > maxLength) {
+        let shortenedTitle = title.substring(0, maxLength - 3) + '...';  // Обрезаем и добавляем многоточие
+        element.innerText = shortenedTitle;  // Обновляем заголовок элемента
+    }
+}
+
 window.addEventListener('resize',(e) => {
     let windowWidth  = window.innerWidth;
     if (windowWidth  <=  1000)  {
-        forwardButton.innerHTML  =  '<img src="./content/arrow_forward.svg" alt="<">';
+        forwardButton.innerHTML  =  '<img src="./content/arrow_forward.svg" alt=">">';
         backButton.innerHTML  =  '<img src="./content/arrow_back.svg" alt="<">';
     } else {
         backButton.textContent  =  'Назад';
@@ -43,5 +41,31 @@ window.addEventListener('resize',(e) => {
     }
 });
 
+window.addEventListener('resize',(e) => {
+    let windowWidth  = window.innerWidth;
+    if (windowWidth  <=  500)  {
+        shortenTitle(blackHeader, 20);  // Сокращаем заголовок до 20 символов
+        ansverButton.innerHTML  =  '<img src="./content/check_circle.svg" alt=">">';
+        reloadButton.innerHTML  =  '<img src="./content/refresh.svg" alt="<">';
+    } else {
+        ansverButton.textContent  =  'Ответить';
+        reloadButton.textContent  =  'Повторить';
+    }
+});
+
 titleUpper.innerHTML = title_of_eom;
 blackHeader.innerHTML = title_of_eom;
+
+
+
+function adjustContentWrapper() {
+    const headerHeight = document.getElementById('header').offsetHeight;
+    const footerHeight = document.getElementById('footer').offsetHeight;
+    const contentWrapper = document.getElementById('contentWrapper');
+
+    contentWrapper.style.paddingTop = headerHeight + 'px';
+    contentWrapper.style.paddingBottom = footerHeight + 'px';
+}
+
+window.addEventListener('load', adjustContentWrapper);
+window.addEventListener('resize', adjustContentWrapper);
